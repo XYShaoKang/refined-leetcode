c4fbed030e48ade13c32af08db56d284bcdd41cc
XYShaoKang <38753204+XYShaoKang@users.noreply.github.com>
2022-02-24 17:46:52 +0800

    chore: 修改注释

--- src/content/load.ts
+++ src/content/load.ts
@@ -20,7 +20,7 @@ function awaitReload() {
 function loadScript(url: string) {
   const script = document.createElement('script')
 
-  // 将扩展 id 添加到脚本中,方便网页脚本通过 id 跟扩展通讯
+  // 将扩展 id 添加到脚本标签中,方便网页脚本通过 id 跟扩展通讯
   script.id = 'leetcode-extension'
   script.dataset.extensionid = chrome.runtime.id
   script.src = url
