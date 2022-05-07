e232a9beb807a2df26d4ef3ef60c08574710c183
XYShaoKang <38753204+XYShaoKang@users.noreply.github.com>
2022-05-07 14:26:18 +0800

    fix(problems): 修复页面内跳转
    现在进行客户端路由跳转时,计时组件会执行对应的逻辑
    当从一题跳转到另外一题时,会自动加载计时组件
    如果是在同一题内跳转,则不做操作继续计时

--- src/content/pages/problems/index.tsx
+++ src/content/pages/problems/index.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
-import { render } from 'react-dom'
+import ReactDOM, { render } from 'react-dom'
 
 import Clock from './Clock'
 import { findElement } from '../../utils'
 
+let root: HTMLDivElement | null = null
+let titleSlug = ''
+
 async function load() {
+  titleSlug = location.pathname.split('/').filter(Boolean)[1]
   const parent = await findElement('.container__Kjnx>.action__KaAP')
   if (parent && parent instanceof HTMLElement) {
-    const root = document.createElement('div')
+    root = document.createElement('div')
     parent.prepend(root)
     root.style.display = 'flex'
     root.style.alignItems = 'center'
@@ -25,3 +29,40 @@ async function load() {
 if (/\/problems\//.test(location.pathname)) {
   load()
 }
+
+const urlMatchReg = /\/problems\//
+
+function unmount() {
+  if (root) {
+    ReactDOM.unmountComponentAtNode(root)
+    root = null
+    titleSlug = ''
+  }
+}
+
+window.addEventListener('urlchange', async function () {
+  /**
+   * url 变化,可能会有四种情况:
+   * 1. 从不匹配的地址跳转到匹配的地址
+   * 2. 从匹配的地址跳转到不匹配的地址
+   * 3. 从匹配的地址跳转到匹配的地址
+   * 4. 从不匹配的地址跳转到不匹配的地址
+   *
+   * 其中需要做处理的是
+   * 第一种情况需要加载组件
+   * 第二种情况需要卸载组件
+   * 而第三第四种清理可以不用处理
+   */
+
+  if (!urlMatchReg.test(location.pathname)) {
+    // 从答题页跳转到非答题页时,卸载计时组件
+    unmount()
+  } else {
+    // 在答题页之间相互跳转,如果还是在同一题,则不做任何操作,如果是跳转另外一题则重新开始计时
+    const curSlug = location.pathname.split('/').filter(Boolean)[1]
+    if (curSlug !== titleSlug) {
+      unmount()
+      load()
+    }
+  }
+})
