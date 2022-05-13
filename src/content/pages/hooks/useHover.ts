7114d13e3fb401ac4f1bc1380975224cbcbfd515
XYShaoKang <38753204+XYShaoKang@users.noreply.github.com>
2022-05-13 13:43:59 +0800

    fix(useHook): 处理当 ref 发生变化的逻辑

--- src/content/pages/hooks/useHover.ts
+++ src/content/pages/hooks/useHover.ts
@@ -1,4 +1,14 @@
-import { useEffect, useRef, useState } from 'react'
+import { useCallback, useRef, useState } from 'react'
+import { useEvent } from './useEvent'
+
+let timer: ReturnType<typeof setTimeout> | undefined
+
+function clearTimer() {
+  if (timer !== undefined) {
+    clearTimeout(timer)
+    timer = undefined
+  }
+}
 
 /**
  * 处理 hover 逻辑的钩子
@@ -6,32 +16,37 @@ import { useEffect, useRef, useState } from 'react'
  */
 export const useHover = <T extends HTMLElement>(
   delay = 0
-): [ref: React.RefObject<T>, hover: boolean] => {
-  const ref = useRef<T>(null)
+): [ref: (el: T | null) => void, hover: boolean] => {
+  const ref = useRef<T | null>()
   const [hover, setHover] = useState(false)
 
-  useEffect(() => {
-    let timer: ReturnType<typeof setTimeout>
-    const handleMouseOver = (_e: MouseEvent) => {
-      setHover(true)
+  const handleMouseOver = useEvent((_e: MouseEvent) => {
+    setHover(true)
+    clearTimer()
+  })
 
-      if (timer !== undefined) {
-        clearTimeout(timer)
-      }
+  const handleMouseOut = useEvent((_e: MouseEvent) => {
+    timer = setTimeout(() => setHover(false), delay)
+  })
+
+  const refcb = useCallback((el: T | null) => {
+    if (ref.current) {
+      // ref 会在卸载时被设置为 null
+      // 所以不用另外在 useEffect 去设置针对组件卸载时删除事件侦听器的逻辑,只要在这里设置即可
+
+      clearTimer()
+      setHover(false)
+
+      ref.current.removeEventListener('mouseover', handleMouseOver)
+      ref.current.removeEventListener('mouseout', handleMouseOut)
     }
-    const handleMouseOut = (_e: MouseEvent) => {
-      timer = setTimeout(() => setHover(false), delay)
-    }
+
+    ref.current = el
     if (ref.current) {
       ref.current.addEventListener('mouseover', handleMouseOver)
       ref.current.addEventListener('mouseout', handleMouseOut)
     }
-    return () => {
-      if (ref.current) {
-        ref.current.removeEventListener('mouseover', handleMouseOver)
-        ref.current.removeEventListener('mouseout', handleMouseOut)
-      }
-    }
   }, [])
-  return [ref, hover]
+
+  return [refcb, hover]
 }
