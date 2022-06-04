6799dd109cbff0d54a2c00e0c4a63c29a69ac13c
XYShaoKang <38753204+XYShaoKang@users.noreply.github.com>
2022-06-04 19:37:09 +0800

    perf: 添加超时设置

--- src/background/utils.ts
+++ src/background/utils.ts
@@ -52,16 +52,34 @@ export type MyRankingType = {
   my_rank: RankType
 }
 
+/**
+ * 根据 key 缓存函数执行结果
+ * @param keyFn 生成缓存的  key
+ * @param handle 输出函数
+ * @param timeout 超时设置,单位为毫秒,默认为 10 分钟,如果缓存数据超过 timeout,则重新获取结果
+ * @returns
+ */
 function cache<T extends any[], R>(
   keyFn: (...args: T) => string,
-  fn: (...args: T) => Promise<R>
+  handle: (...args: T) => Promise<R>,
+  timeout = 600000
 ): (...args: T) => Promise<R> {
-  const map = new Map<string, Promise<R>>()
-  return (...args: T) => {
+  const map = new Map<string, [Promise<R>, number]>()
+  return async (...args: T) => {
     const key = keyFn(...args)
-    if (map.has(key)) return map.get(key)!
-    const promise = fn(...args)
-    map.set(key, promise)
+    if (map.has(key)) {
+      const [promise, time] = map.get(key)!
+      if (new Date().valueOf() - time <= timeout) {
+        try {
+          const data = await promise
+          return data
+        } catch (error) {
+          // 如果当前缓存有错误,则舍弃缓存的结果,重新获取
+        }
+      }
+    }
+    const promise = handle(...args)
+    map.set(key, [promise, new Date().valueOf()])
     return promise
   }
 }
