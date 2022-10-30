249126d1019f3b8f9b3e8a27accf0701475bbdd7
XYShaoKang <38753204+XYShaoKang@users.noreply.github.com>
2022-10-30 20:23:26 +0800

    fix(ranking): 正确捕获因网络原因导致的 fetch 请求错误

--- src/background/utils/predictorApi.ts
+++ src/background/utils/predictorApi.ts
@@ -47,16 +47,24 @@ export const predictorApi = cache(
     url.searchParams.set('contestId', contestId)
     url.searchParams.set('handles', handles.join(';'))
 
-    const res = await fetch(url.toString())
-    if (res.status === 200) {
-      return res.json()
-    } else if (retry < RETRY_COUNT) {
-      // 换一个 API 试试
+    try {
+      const res = await fetch(url.toString())
+
+      if (res.status === 200) {
+        return res.json()
+      } else if (retry < RETRY_COUNT) {
+        // 换一个 API 试试
+        changeApiIndex()
+        await sleep(2 ** retry * 100)
+        return predictorApi({ contestId, handles }, retry + 1)
+      } else {
+        throw new Error('获取预测数据失败')
+      }
+    } catch (error) {
+      // 网络请求失败，换一个 API 试试
       changeApiIndex()
       await sleep(2 ** retry * 100)
       return predictorApi({ contestId, handles }, retry + 1)
-    } else {
-      throw new Error('获取预测数据失败')
     }
   }
 )
